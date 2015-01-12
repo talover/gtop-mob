dab6c81c3a8821111846ebb4fffae43f8bf32a5c
talover <taloer9@gmail.com>
2015-01-12 13:34:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$('.logger-btn').click(function(e){
+		e.preventDefault();
+		$(this).parents('#logger').toggleClass('active').find('.logger-dropdown').slideToggle();
+		e.stopPropagation();
+	});
+
+	$(document).click(function(){
+		if($('.logger-dropdown').is(":visible")) {
+			$('.logger-dropdown').slideUp();
+			$('#logger').removeClass('active');
+		}
+	});
+
+	$('#menu-button').click(function(){$('#main,body').toggleClass('active');});
+
+	with_video();
+	$('[data-toggle="tooltip"]').tooltip()
+	
+	/* -- gold gift --*/
+
+	$('.gold-gift').click(function(){
+		$(this).toggleClass('active');
+		$('.gold-wrap').toggleClass('active');
+	});
+});
+
+
+$(window).resize(function(){
+	with_video();
+});
+
+/* for video block */
+function with_video(){
+	$('.article .with-video').each(function(){
+		var hv = $(this).find('.f-left img').height();
+
+		$(this).find('li').css('height', ((parseInt(hv)/2)) + "px");
+	});
+}
